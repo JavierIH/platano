c2b43a3965f281755b6d0031182fc7f51ef82627
KFRodicio <kfrodicio@gmail.com>
2015-05-03 05:39:30 +0200

    New method to define origin and goal

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -18,9 +18,6 @@ def main():
 
     image_to_load = 'environment_test.png'
 
-    start = 46
-    goal = 55
-
     # Create collision checker
     simple_collision_checker = SimpleCollisionChecker()
     circle_collision_checker = CircleCollisionChecker((400, 640), 10)
@@ -28,6 +25,34 @@ def main():
 
     env = Environment(image_to_load, circle_collision_checker)
     cv2.imshow("Loaded img", env.image)
+
+    # Ask for the initial point and the goal point
+    print("Los limites del mapa son: ", env.x_limit, env.y_limit)
+
+    i_point = np.zeros((2, 1))
+    i_point[0] = int(input("Introduzca la coordenada x del punto inicial:"))
+    i_point[1] = int(input("Introduzca la coordenada y del punto inicial:"))
+    start = tuple(i_point)
+    valid_start = env.is_valid(start)
+
+    while start[0] < 0 or start[0] > env.x_limit or start[1] < 0 or start [1] > env.y_limit or valid_start == False:
+        print("el punto seleccionado no es valido")
+        start[0] = int(input("Introduzca la coordenada x del punto inicial:"))
+        start[1] = int(input("Introduzca la coordenada y del punto inicial:"))
+        valid_start = env.is_valid(start)
+
+    g_point = np.zeros((2, 1))
+    g_point[0] = int(input("Introduzca la coordenada x del punto final:"))
+    g_point[1] = int(input("Introduzca la coordenada y del punto final:"))
+    goal = tuple(g_point)
+    valid_goal = env.is_valid(goal)
+
+    while goal[0] < 0 or goal[0] > env.x_limit or goal[1] < 0 or goal[1] > env.y_limit or valid_goal == False:
+        print("el punto seleccionado no es valido")
+        goal[0] = int(input("Introduzca la coordenada x del punto final:"))
+        goal[1] = int(input("Introduzca la coordenada y del punto final:"))
+        valid_goal = env.is_valid(goal)
+
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
@@ -67,6 +92,8 @@ def main():
 
     # Generate graph:
     points = hammersley_points
+    points[0] = (int(start[0]), int(start[1]))
+    points[len(points)-1] = (int(goal[0]), int(goal[1]))
     threshold = 50
     size = len(points)
     distance_matrix = np.zeros((size, size))
@@ -96,12 +123,12 @@ def main():
 
     #print distance_matrix
 
-    path = a_algorithm(start, goal, points, distance_matrix)
+    path = a_algorithm(0, len(points)-1, points, distance_matrix)
     useless_node = True
     aux = len(path)-1
     aux2 = aux - 1
 
-    #erase the useless nodes in the path
+    # erase the useless nodes in the path
     while aux > 0:
         while useless_node and aux2 > 0:
             aux2 -= 1
@@ -120,8 +147,8 @@ def main():
         cv2.line(show, origin, end, (0, 0, 255))
     for point in points:
         cv2.circle(show, point, 2, (255, 0, 0), 2)
-    cv2.circle(show, points[start], 2, (0, 255, 0), 2)
-    cv2.circle(show, points[goal], 2, (255, 255, 255), 2)
+    cv2.circle(show, points[0], 2, (0, 255, 0), 2)
+    cv2.circle(show, points[len(points)-1], 2, (255, 255, 255), 2)
     cv2.imshow("Path", show)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
