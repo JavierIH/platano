e8baa0dcbeb6dd49152274f33bc9724b70322781
David-Estevez <david.estevez.fdez@gmail.com>
2015-05-03 02:13:23 +0200

    Add new collision checker based on circles instead of points

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -5,6 +5,7 @@ from RandomNodeGenerator import RandomNodeGenerator
 from HammersleyNodeGenerator import HammersleyNodeGenerator
 
 from SimpleCollisionChecker import SimpleCollisionChecker
+from CircleCollisionChecker import CircleCollisionChecker
 
 from Environment import Environment
 
@@ -19,19 +20,23 @@ def main():
 
     start = 46
     goal = 55
-    # Create collision checker
-    collision_checker = SimpleCollisionChecker()
 
-    env = Environment(image_to_load, collision_checker)
-    #cv2.imshow("Loaded img", env.image)
+    # Create collision checker
+    simple_collision_checker = SimpleCollisionChecker()
+    circle_collision_checker = CircleCollisionChecker((400, 640), 10)
+    collision_checker = simple_collision_checker
+
+    env = Environment(image_to_load, circle_collision_checker)
+    cv2.imshow("Loaded img", env.image)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
+
 
     # Find obstacles
     #----------------------------------
     show = cv2.cvtColor(env.image, cv2.COLOR_GRAY2BGR)
-    #cv2.drawContours(show, env.obstacles, -1, (0, 0, 255), 3)
-    #cv2.imshow("Obstacles", show)
+    cv2.drawContours(show, env.obstacles, -1, (0, 0, 255), 3)
+    cv2.imshow("Obstacles", show)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
@@ -47,7 +52,7 @@ def main():
     for point in random_points:
         cv2.circle(show, point, 2, (0, 255, 0), 1)
     cv2.drawContours(show, env.obstacles, -1, (0, 0, 255), 3)
-    #cv2.imshow("Random points", show)
+    cv2.imshow("Random points", show)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
@@ -56,12 +61,11 @@ def main():
     for point in hammersley_points:
         cv2.circle(show, point, 2, (255, 0, 0), 2)
     cv2.drawContours(show, env.obstacles, -1, (0, 0, 255), 3)
-    #cv2.imshow("hammersley points", show)
+    cv2.imshow("hammersley points", show)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
     # Generate graph:
-    # --------------------------------------------------------------
     points = hammersley_points
     threshold = 50
     size = len(points)
@@ -87,25 +91,25 @@ def main():
                 end = points[j]
                 cv2.line(show, origin, end, (255, 255, 0))
     cv2.imshow("Connections", show)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     #print distance_matrix
 
-    path = a_algorithm(start, goal, points, distance_matrix)
-
-    show = cv2.cvtColor(env.image, cv2.COLOR_GRAY2BGR)
-    for i in range(len(path)-1):
-        origin = points[path[i]]
-        end = points[path[i+1]]
-        cv2.line(show, origin, end, (0, 0, 255))
-    for point in points:
-        cv2.circle(show, point, 2, (255, 0, 0), 2)
-    cv2.circle(show, points[start], 2, (0, 255, 0), 2)
-    cv2.circle(show, points[goal], 2, (255, 255, 255), 2)
-    cv2.imshow("Path", show)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # path = a_algorithm(start, goal, points, distance_matrix)
+    #
+    # show = cv2.cvtColor(env.image, cv2.COLOR_GRAY2BGR)
+    # for i in range(len(path)-1):
+    #     origin = points[path[i]]
+    #     end = points[path[i+1]]
+    #     cv2.line(show, origin, end, (0, 0, 255))
+    # for point in points:
+    #     cv2.circle(show, point, 2, (255, 0, 0), 2)
+    # cv2.circle(show, points[start], 2, (0, 255, 0), 2)
+    # cv2.circle(show, points[goal], 2, (255, 255, 255), 2)
+    # cv2.imshow("Path", show)
+    # #cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 
