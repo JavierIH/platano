ed3708ba97586957fd454618c30003e703ce9a10
David-Estevez <david.estevez.fdez@gmail.com>
2015-05-07 01:29:47 +0200

    Start refactoring planner

--- algorithm/Planner.py
+++ algorithm/Planner.py
@@ -1,5 +1,6 @@
 from RandomNodeGenerator import RandomNodeGenerator
 from HammersleyNodeGenerator import HammersleyNodeGenerator
+from HaltonNodeGenerator import HaltonNodeGenerator
 
 from SimpleCollisionChecker import SimpleCollisionChecker
 from CircleCollisionChecker import CircleCollisionChecker
@@ -8,6 +9,8 @@ from Environment import Environment
 from DilatedEnvironment import DilatedEnvironment
 
 from a_algorithm import a_algorithm
+
+import numpy as np
 
 __author__ = 'def'
 
@@ -30,13 +33,84 @@ class Planner:
         :param collision_radius: in methods using this parameter, this is the radius of the
             robot
         """
-        pass
+        # Configure planner:
+        self.environment_image = environment_image
+        self.n_nodes = nodes
+        self.threshold_neighbors = threshold_neighbors
 
-    def find_path(self, node_origin, node_destination):
+        # Node generation
+        self.node_generator = None
+        if node_gen_method == 'random':
+            self.node_generator = RandomNodeGenerator()
+        elif node_gen_method == 'Hammersley':
+            self.node_generator = HammersleyNodeGenerator()
+        elif node_gen_method == 'Hammersley':
+            self.node_generator = HaltonNodeGenerator()
+        else:
+            raise Exception('Node generator requested (%s) not found!'%node_gen_method)
+
+        # Collision checker
+        self.collision_checker = None
+        self.environment = None
+
+        if collision_method == 'simple' or collision_method == 'dilate':
+            self.collision_checker = SimpleCollisionChecker()
+        elif collision_method == 'circle':
+            self.collision_checker = CircleCollisionChecker(environment_image.shape[:2], collision_radius)
+        else:
+            raise Exception('Collision method requested (%s) not found!'%collision_method)
+
+        if collision_method == 'simple' or collision_method == 'circle':
+            self.environment = Environment(self.environment_image, self.collision_checker)
+        elif collision_method == 'dilate':
+            self.environment = DilatedEnvironment(self.environment_image, self.collision_checker, collision_radius)
+
+        # Generate nodes:
+        self.nodes = self.node_generator.generatenodes(self.environment, self.n_nodes)
+
+        #points[0] = (int(start[0]), int(start[1]))
+        #points[len(points)-1] = (int(goal[0]), int(goal[1]))
+        size = len(self.nodes)
+        self.distance_matrix = np.zeros((size, size))
+        for i, origin in enumerate(self.nodes):
+            for j, end in enumerate(self.nodes):
+                dist = np.linalg.norm(np.array(origin) - np.array(end))
+                if dist <= self.threshold_neighbors and self.environment.is_line_valid(origin, end):
+                    self.distance_matrix[i, j] = dist
+                else:
+                    self.distance_matrix[i, j] = -1
+
+
+    def find_path(self, node_origin, node_goal):
         """
-        Finds the shortest path between node_origin and node_destination
+        Finds the shortest path between node_origin and node_goal
         :return: A list of points containing the shortest path
         """
+
+        # Put origin and goal in the graph
+
+        # Calculate shortest path using A*
+        path = a_algorithm(0, len(self.nodes)-1, self.nodes, self.distance_matrix)
+
         return []
 
+    def find_path_and_simplify(self, node_origin, node_goal):
+        path = self.find_path(node_origin, node_goal)
+        useless_node = True
+        aux = len(path)-1
+        aux2 = aux - 1
 
+        # erase the useless nodes in the path
+        while aux > 0:
+            while useless_node and aux2 > 0:
+                aux2 -= 1
+                useless_node = self.environment.is_line_valid(points[path[aux]], points[path[aux2]])
+                if useless_node:
+                    del path[aux2+1]
+                    aux -= 1
+            aux = aux2
+            aux2 -= 1
+            useless_node = True
+
+if __name__ == '__main__':
+    pass
